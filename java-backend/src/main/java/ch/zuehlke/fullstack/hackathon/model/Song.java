c1d8a3f7bf2c4fa9a7a2294fa3bf2ac6ee6858c9
Philippe Willi <philippe.willi@zuehlke.com>
2024-03-18 16:10:30 +0100

    feat: add Song

--- java-backend/src/main/java/ch/zuehlke/fullstack/hackathon/model/Song.java
+++ java-backend/src/main/java/ch/zuehlke/fullstack/hackathon/model/Song.java
@@ -1,11 +1,12 @@
 package ch.zuehlke.fullstack.hackathon.model;
 
 import java.util.List;
+import java.util.UUID;
 
-public record CreateSongDto(
+public record Song(
+        UUID id,
         String topic,
         String genre,
         List<String> instruments,
-        String mood
-) {
+        String mood) {
 }

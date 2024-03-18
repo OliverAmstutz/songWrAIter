65824ce507a0bb2740eadcef1cee09ed5bab5219
Philippe Willi <philippe.willi@zuehlke.com>
2024-03-18 15:47:39 +0100

    feat: add CreateSongForm

---
+++ react-frontend/src/components/CreateSongForm.tsx
@@ -0,0 +1,49 @@
+import {SubmitHandler, useForm} from "react-hook-form"
+
+interface IFormInput {
+    topic: string
+    genre: string
+    instruments: string[]
+    mood: string
+}
+
+export default function CreateSongForm() {
+    const {register, handleSubmit} = useForm<IFormInput>({
+        defaultValues: {
+            topic: '',
+            genre: '',
+            instruments: [],
+            mood: '',
+        },
+    })
+    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label>Topic</label>
+            <input {...register("topic")} />
+
+            <label>Genre</label>
+            <select {...register("genre")} defaultValue={""}>
+                <option value="rock">rock</option>
+                <option value="blues">blues</option>
+                <option value="pop">pop</option>
+            </select>
+
+            <label>Instruments</label>
+            <select {...register("instruments")}>
+                <option value="guitar">guitar</option>
+                <option value="piano">piano</option>
+                <option value="drums">drums</option>
+            </select>
+
+            <label>Mood</label>
+            <select {...register("mood")}>
+                <option value="sad">sad</option>
+                <option value="happy">happy</option>
+                <option value="neutral">neutral</option>
+            </select>
+
+            <input type="submit"/>
+        </form>)
+}
--- react-frontend/src/components/SongPage.tsx
+++ react-frontend/src/components/SongPage.tsx
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
+import CreateSongForm from "./CreateSongForm.tsx";
 
 const Section = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `;
 
 export default function SongPage() {
     return (
         <Section>
-            <h1>Hello from the other Side</h1>
+            <CreateSongForm/>
         </Section>
     );
 }
